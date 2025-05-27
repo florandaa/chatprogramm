9ae9b611ae9bcb1c1686771495bf23dd28a504c6
florandaa <floranda.3010@gmail.com>
2025-05-27 15:14:22 +0200

    JOIN  und time.sleep

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ cfg = load_config()
 tcp_port = cfg["port"][0]
 udp_port = cfg["port"][1]
 handle = cfg["handle"]
+whoisport = cfg["whoisport"]
 
+# Starte UDP- und TCP-Server im Hintergrund
 threading.Thread(target=udp_listener, args=(udp_port,), daemon=True).start()
 threading.Thread(target=tcp_server, args=(tcp_port,), daemon=True).start()
 
+# NEU: JOIN an Discovery senden
+udp_send(f"JOIN {handle} {tcp_port}", "127.0.0.1", whoisport)
+
+# Testnachrichten
+time.sleep(1)
 udp_send(f"{handle} sagt hallo über UDP", "127.0.0.1", udp_port)
 tcp_send("Hallo über TCP", "127.0.0.1", tcp_port)
